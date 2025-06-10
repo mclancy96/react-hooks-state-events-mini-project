bd3744e06b83113ebaeb3dc47f5324ab3be006d2
Mike <mclancy96@gmail.com>
2025-06-10 15:57:13 -0400

    Displaying and deleting tasks is working

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,18 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [taskList, setTaskList] = useState(TASKS)
+
+  const removeTask = (taskText) => {
+    setTaskList(taskList.filter(task => task.text !== taskText))
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={taskList} removeTask={removeTask} />
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Task() {
+function Task({ task, removeTask }) {
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{task.category}</div>
+      <div className="text">{task.text}</div>
+      <button className="delete" onClick={() => { removeTask(task.text) }}>X</button>
     </div>
   );
 }
